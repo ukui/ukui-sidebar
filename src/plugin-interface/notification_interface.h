5431867383d57b0dc823421b8047942e05ba50fd
chenchunan <chenchunan@kylinos.cn>
2020-02-13 15:22:35 +0800

    add a little modification

--- src/plugin-interface/notification_interface.h
+++ src/plugin-interface/notification_interface.h
@@ -26,12 +26,11 @@ class NotificationInterface
 {
 public:
     virtual ~NotificationInterface() {}
-//    virtual void set_text() = 0;
-//    virtual QPushButton* create_pushbutton(QString &button_name) = 0;
-//    virtual QTextEdit* create_lineedit() = 0;
 
     //返回一个Widget设置到centerwidget中进行显示
     virtual QWidget* centerWidget() = 0;
+    //更新通知中心通知列表或收纳列表的推送时间显示
+    virtual void updatePushTime() = 0;
 //    virtual void Notify(QString strAppName, QString strIconPath, QString strSummary, QString strTime, QString strBody) = 0;
 };
 
