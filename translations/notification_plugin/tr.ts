4d960d4bb8e93e44e3ec965d4fdbb93168befa5b
Aylinux <cihanalk@gmail.com>
2020-05-21 06:03:21 +0300

    Create tr.ts (#181)

---
+++ translations/notification_plugin/tr.ts
@@ -0,0 +1,83 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../src/plugins/notification_plugin/notification_plugin.cpp" line="66"/>
+        <source>Notification center</source>
+        <translation type="unfinished">Bildirim Merkezi</translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/notification_plugin/notification_plugin.cpp" line="107"/>
+        <location filename="../../src/plugins/notification_plugin/notification_plugin.cpp" line="591"/>
+        <source>Important notice</source>
+        <translation type="unfinished">Önemli uyarı</translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/notification_plugin/notification_plugin.cpp" line="115"/>
+        <source>Clean up</source>
+        <translation type="unfinished">Temizlemek</translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/notification_plugin/notification_plugin.cpp" line="122"/>
+        <source>Set up</source>
+        <translation type="unfinished">Kur</translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/notification_plugin/notification_plugin.cpp" line="146"/>
+        <source>No new notifications</source>
+        <translation type="unfinished">Yeni bildirim yok</translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/notification_plugin/notification_plugin.cpp" line="169"/>
+        <source>No unimportant notice</source>
+        <translation type="unfinished">Önemsiz bildirim yok</translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/notification_plugin/notification_plugin.cpp" line="561"/>
+        <source>Unimportant notice</source>
+        <translation type="unfinished">Önemsiz bildirim</translation>
+    </message>
+</context>
+<context>
+    <name>SingleMsg</name>
+    <message>
+        <location filename="../../src/plugins/notification_plugin/singlemsg.cpp" line="117"/>
+        <source>now</source>
+        <translation type="unfinished">şimdi</translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/notification_plugin/singlemsg.cpp" line="281"/>
+        <source>Yesterday </source>
+        <translation type="unfinished">Dün </translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/notification_plugin/singlemsg.cpp" line="360"/>
+        <source>In addition </source>
+        <translation type="unfinished">Ek olarak </translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/notification_plugin/singlemsg.cpp" line="360"/>
+        <source> notification</source>
+        <translation type="unfinished"> bildirim</translation>
+    </message>
+    <message>
+        <source>In addition</source>
+        <translation type="obsolete">tür</translation>
+    </message>
+</context>
+<context>
+    <name>TakeInBoxToolButton</name>
+    <message>
+        <location filename="../../src/plugins/notification_plugin/takeinboxtoolbutton.cpp" line="36"/>
+        <source>Enter unimportant news</source>
+        <translation type="unfinished">Önemsiz haberlere girin</translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/notification_plugin/takeinboxtoolbutton.cpp" line="41"/>
+        <source>Quit unimportant news</source>
+        <translation type="unfinished">Önemsiz haberlerden çıkın</translation>
+    </message>
+</context>
+</TS>
