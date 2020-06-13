cf9bef802ad02bdf203a0689a4fc3eff069a57f0
Aylinux <cihanalk@gmail.com>
2020-06-13 14:14:04 +0300

    Create tr.ts

---
+++ src/tr.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="main.cpp" line="60"/>
+        <source>Display debug information</source>
+        <translation type="unfinished">Hata ayıklama bilgilerini görüntüle</translation>
+    </message>
+    <message>
+        <location filename="widget.cpp" line="182"/>
+        <source>Open</source>
+        <translation type="unfinished">Aç</translation>
+    </message>
+    <message>
+        <location filename="widget.cpp" line="185"/>
+        <source>Set up notification center</source>
+        <translation type="unfinished">Bildirim merkezini ayarla</translation>
+    </message>
+</context>
+<context>
+    <name>Widget</name>
+    <message>
+        <location filename="widget.cpp" line="218"/>
+        <source>Sidebar</source>
+        <translation type="unfinished">Kenar çubuğu</translation>
+    </message>
+</context>
+<context>
+    <name>sidebarPluginsWidgets</name>
+    <message>
+        <location filename="sidebarpluginswidgets.cpp" line="92"/>
+        <source>Clipboard</source>
+        <translation type="unfinished">Pano</translation>
+    </message>
+    <message>
+        <location filename="sidebarpluginswidgets.cpp" line="98"/>
+        <source>Plugins</source>
+        <translation type="unfinished">Eklentiler</translation>
+    </message>
+</context>
+</TS>
