7338101c669029803e190ee78a39b4c752f530c4
Aylinux <cihanalk@gmail.com>
2020-07-03 22:18:53 +0300

    Create notebook_tr.ts

---
+++ src/plugins/ukui_notebook/notebook_tr.ts
@@ -0,0 +1,292 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr_TR">
+<context>
+    <name>Edit_page</name>
+    <message>
+        <location filename="edit_page.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="edit_page.cpp" line="49"/>
+        <source>ukui-memo</source>
+        <translatorcomment></translatorcomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="edit_page.cpp" line="751"/>
+        <source>Close</source>
+        <translatorcomment>Kapat</translatorcomment>
+        <translation>Kapat</translation>
+    </message>
+    <message>
+        <location filename="edit_page.cpp" line="753"/>
+        <source>Open Notepad</source>
+        <translatorcomment>Notepad Aç</translatorcomment>
+        <translation>Notepad Aç</translation>
+    </message>
+    <message>
+        <source>New</source>
+        <translatorcomment>Yeni</translatorcomment>
+        <translation type="vanished">Yeni</translation>
+    </message>
+</context>
+<context>
+    <name>HeaderBar</name>
+    <message>
+        <location filename="headerbar.cpp" line="65"/>
+        <source>View Type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="headerbar.cpp" line="94"/>
+        <source>Sort Type</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="widget.cpp" line="678"/>
+        <location filename="widget.cpp" line="1026"/>
+        <location filename="widget.cpp" line="1035"/>
+        <source>%1 records in total</source>
+        <translatorcomment>Toplam %1 kayıt</translatorcomment>
+        <translation>Toplam %1 kayıt</translation>
+    </message>
+</context>
+<context>
+    <name>Set_font_color_page</name>
+    <message>
+        <location filename="set_font_color_page.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Set_font_size_page</name>
+    <message>
+        <location filename="set_font_size_page.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Text_editing</name>
+    <message>
+        <location filename="text_editing.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="text_editing.ui" line="321"/>
+        <source>22</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Widget</name>
+    <message>
+        <location filename="widget.ui" line="14"/>
+        <source>Widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widget.ui" line="69"/>
+        <source>New</source>
+        <translatorcomment>Yeni</translatorcomment>
+        <translation>Yeni</translation>
+    </message>
+    <message>
+        <location filename="widget.cpp" line="225"/>
+        <source>ukui-note</source>
+        <translatorcomment>ukui-not</translatorcomment>
+        <translation>ukui-not</translation>
+    </message>
+    <message>
+        <location filename="widget.cpp" line="232"/>
+        <source>Search</source>
+        <translatorcomment>Ara</translatorcomment>
+        <translation>Ara</translation>
+    </message>
+    <message>
+        <location filename="widget.cpp" line="412"/>
+        <source>Create New Note</source>
+        <translatorcomment>Yeni Not Oluştur</translatorcomment>
+        <translation>Yeni Not Oluştur</translation>
+    </message>
+    <message>
+        <location filename="widget.cpp" line="413"/>
+        <source>Delete Selected Note</source>
+        <translatorcomment>Seçilen Notu Sil</translatorcomment>
+        <translation>Seçilen Notu Sil</translation>
+    </message>
+    <message>
+        <location filename="widget.cpp" line="414"/>
+        <source>Switch View</source>
+        <translatorcomment>Görünümü Değiştir</translatorcomment>
+        <translation>Görünümü Değiştir</translation>
+    </message>
+    <message>
+        <location filename="widget.cpp" line="415"/>
+        <source>Sort</source>
+        <translatorcomment>Sırala</translatorcomment>
+        <translation>Sırala</translation>
+    </message>
+    <message>
+        <location filename="widget.cpp" line="416"/>
+        <source>Switching Themes</source>
+        <translatorcomment>Tema Değiştir</translatorcomment>
+        <translation>Tema Değiştir</translation>
+    </message>
+    <message>
+        <location filename="widget.cpp" line="417"/>
+        <source>Exit</source>
+        <translatorcomment>Çıkış</translatorcomment>
+        <translation>Çıkış</translation>
+    </message>
+    <message>
+        <location filename="widget.cpp" line="418"/>
+        <source>Mini</source>
+        <translatorcomment></translatorcomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="widget.cpp" line="726"/>
+        <source>Welcome to use Kylin system, you will experience more intimate service.</source>
+        <translatorcomment>Kylin sistemini kullanmaya hoş geldiniz, daha samimi bir hizmet göreceksiniz.</translatorcomment>
+        <translation>Kylin sistemini kullanmaya hoş geldiniz, daha samimi bir hizmet göreceksiniz.</translation>
+    </message>
+</context>
+<context>
+    <name>emptyNotes</name>
+    <message>
+        <location filename="emptyNotes.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="emptyNotes.ui" line="44"/>
+        <source>确定要清空便签本吗?</source>
+        <translation type="unfinished">Yapışkan notu silmek istediğinizden emin misiniz?</translation>
+    </message>
+    <message>
+        <location filename="emptyNotes.ui" line="64"/>
+        <source>不再询问</source>
+        <translation type="unfinished">Artık sorma</translation>
+    </message>
+    <message>
+        <location filename="emptyNotes.ui" line="81"/>
+        <source>取消</source>
+        <translation type="unfinished">İptal et</translation>
+    </message>
+    <message>
+        <location filename="emptyNotes.ui" line="98"/>
+        <source>确定</source>
+        <translation type="unfinished">Onayla</translation>
+    </message>
+</context>
+<context>
+    <name>fontButton</name>
+    <message>
+        <location filename="fontButton.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>iconViewModeDelegate</name>
+    <message>
+        <location filename="iconviewmodedelegate.cpp" line="343"/>
+        <source>Today </source>
+        <translatorcomment>Bugün</translatorcomment>
+        <translation>Bugün</translation>
+    </message>
+    <message>
+        <location filename="iconviewmodedelegate.cpp" line="347"/>
+        <source>Yesterday </source>
+        <translatorcomment>Dün</translatorcomment>
+        <translation>Dün</translation>
+    </message>
+</context>
+<context>
+    <name>listViewModeDelegate</name>
+    <message>
+        <location filename="listViewModeDelegate.cpp" line="373"/>
+        <source>Today </source>
+        <translatorcomment>Bugün</translatorcomment>
+        <translation>Bugün</translation>
+    </message>
+    <message>
+        <location filename="listViewModeDelegate.cpp" line="377"/>
+        <source>Yesterday </source>
+        <translatorcomment>Dün</translatorcomment>
+        <translation>Dün</translation>
+    </message>
+</context>
+<context>
+    <name>noteExitWindow</name>
+    <message>
+        <location filename="noteExitWindow.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="noteExitWindow.ui" line="44"/>
+        <source>Are you sure to exit the note book?</source>
+        <translatorcomment>Not defterinden çıkmak istediğinizden emin misiniz?</translatorcomment>
+        <translation>Not defterinden çıkmak istediğinizden emin misiniz?</translation>
+    </message>
+    <message>
+        <location filename="noteExitWindow.ui" line="64"/>
+        <source>Close the desktop note page at the same time</source>
+        <translatorcomment>Masaüstü not sayfasını aynı anda kapatın</translatorcomment>
+        <translation>Masaüstü not sayfasını aynı anda kapatın</translation>
+    </message>
+    <message>
+        <location filename="noteExitWindow.ui" line="81"/>
+        <source>No</source>
+        <translatorcomment>Hayır</translatorcomment>
+        <translation>Hayır</translation>
+    </message>
+    <message>
+        <location filename="noteExitWindow.ui" line="98"/>
+        <source>Yes</source>
+        <translatorcomment>Evet</translatorcomment>
+        <translation>Evet</translation>
+    </message>
+</context>
+<context>
+    <name>noteHead</name>
+    <message>
+        <location filename="noteHead.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>noteHeadMenu</name>
+    <message>
+        <location filename="noteHeadMenu.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>paletteButton</name>
+    <message>
+        <location filename="paletteButton.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>select_color_page</name>
+    <message>
+        <location filename="select_color_page.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
