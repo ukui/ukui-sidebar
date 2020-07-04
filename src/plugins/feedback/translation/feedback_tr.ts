1ba9e96c50a8195102b1d6acf0f2235c1f0d2464
Aylinux <cihanalk@gmail.com>
2020-07-04 09:38:04 +0300

    Create feedback_tr.ts (#235)

---
+++ src/plugins/feedback/translation/feedback_tr.ts
@@ -0,0 +1,270 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr_TR">
+<context>
+    <name>QObject</name>
+    <message>
+        <source>用户反馈</source>
+        <translatorcomment>Geribildirim</translatorcomment>
+        <translation type="vanished">Geribildirim</translation>
+    </message>
+    <message>
+        <source>用户反馈问题的插件</source>
+        <translatorcomment>Geribildirim eklentisi</translatorcomment>
+        <translation type="vanished">Geribildirim eklentisi</translation>
+    </message>
+</context>
+<context>
+    <name>feedback</name>
+    <message>
+        <location filename="../feedback.cpp" line="126"/>
+        <source>用户反馈</source>
+        <translatorcomment>Geribildirim</translatorcomment>
+        <translation>Geribildirim</translation>
+    </message>
+    <message>
+        <location filename="../feedback.cpp" line="154"/>
+        <source>问题反馈</source>
+        <translatorcomment>Geribildirim</translatorcomment>
+        <translation>Geribildirim</translation>
+    </message>
+    <message>
+        <location filename="../feedback.cpp" line="161"/>
+        <source>问题类型</source>
+        <translatorcomment>Backtype</translatorcomment>
+        <translation type="unfinished">Backtype</translation>
+    </message>
+    <message>
+        <location filename="../feedback.cpp" line="168"/>
+        <source>系统问题</source>
+        <translatorcomment>Sistem sorunu</translatorcomment>
+        <translation>Sistem sorunu</translation>
+    </message>
+    <message>
+        <location filename="../feedback.cpp" line="169"/>
+        <source>意见建议</source>
+        <translatorcomment>Öneriler</translatorcomment>
+        <translation>Öneriler</translation>
+    </message>
+    <message>
+        <location filename="../feedback.cpp" line="170"/>
+        <source>商务合作</source>
+        <translatorcomment>İşbirliği</translatorcomment>
+        <translation>İşbirliği</translation>
+    </message>
+    <message>
+        <location filename="../feedback.cpp" line="171"/>
+        <source>其他</source>
+        <translatorcomment>Diğer</translatorcomment>
+        <translation>Diğer</translation>
+    </message>
+    <message>
+        <location filename="../feedback.cpp" line="183"/>
+        <source>问题描述</source>
+        <translatorcomment>Description</translatorcomment>
+        <translation type="unfinished">Description</translation>
+    </message>
+    <message>
+        <location filename="../feedback.cpp" line="205"/>
+        <source>请输入内容</source>
+        <translatorcomment>Lütfen içerik girin</translatorcomment>
+        <translation>Lütfen içerik girin</translation>
+    </message>
+    <message>
+        <location filename="../feedback.cpp" line="211"/>
+        <source>邮箱</source>
+        <translatorcomment>Email</translatorcomment>
+        <translation type="unfinished">Email</translation>
+    </message>
+    <message>
+        <location filename="../feedback.cpp" line="219"/>
+        <source>邮箱格式输入不正确</source>
+        <translatorcomment>E-posta biçimi yanlış</translatorcomment>
+        <translation>E-posta biçimi yanlış</translation>
+    </message>
+    <message>
+        <location filename="../feedback.cpp" line="225"/>
+        <location filename="../feedback.cpp" line="231"/>
+        <source>*</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../feedback.cpp" line="246"/>
+        <source>上传附件</source>
+        <translatorcomment>Yükle</translatorcomment>
+        <translation>Yükle</translation>
+    </message>
+    <message>
+        <location filename="../feedback.cpp" line="258"/>
+        <source>文件大小不能超过10MB</source>
+        <translatorcomment>Dosya boyutu 10 MB'ı aşamaz</translatorcomment>
+        <translation>Dosya boyutu 10 MB'ı aşamaz</translation>
+    </message>
+    <message>
+        <location filename="../feedback.cpp" line="267"/>
+        <source>浏览...</source>
+        <translatorcomment>Araştır</translatorcomment>
+        <translation>Araştır</translation>
+    </message>
+    <message>
+        <location filename="../feedback.cpp" line="275"/>
+        <source>同意获取我的</source>
+        <translatorcomment>Benimkini al</translatorcomment>
+        <translation>Benimkini al</translation>
+    </message>
+    <message>
+        <location filename="../feedback.cpp" line="285"/>
+        <location filename="../feedback.cpp" line="1187"/>
+        <location filename="../feedback.cpp" line="1326"/>
+        <source>提交</source>
+        <translatorcomment>Sun</translatorcomment>
+        <translation type="unfinished">Sun</translation>
+    </message>
+    <message>
+        <location filename="../feedback.cpp" line="294"/>
+        <source>系统信息</source>
+        <translatorcomment>OS bilgisi</translatorcomment>
+        <translation>OS bilgisi</translation>
+    </message>
+    <message>
+        <location filename="../feedback.cpp" line="347"/>
+        <source>日志文件</source>
+        <translatorcomment>Log dosyası</translatorcomment>
+        <translation>Log dosyası</translation>
+    </message>
+    <message>
+        <location filename="../feedback.cpp" line="354"/>
+        <source>限200字</source>
+        <translatorcomment>200 kelime</translatorcomment>
+        <translation>200 kelime</translation>
+    </message>
+    <message>
+        <location filename="../feedback.cpp" line="370"/>
+        <source>syslog</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../feedback.cpp" line="379"/>
+        <source>apport.log</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../feedback.cpp" line="389"/>
+        <source>dpkg.log</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../feedback.cpp" line="401"/>
+        <source>文件大小超过了10MB或文件格式不支持</source>
+        <translatorcomment>Dosya boyutu 10 MB'ı aşıyor veya dosya biçimi desteklenmiyor</translatorcomment>
+        <translation>Dosya boyutu 10 MB'ı aşıyor veya dosya biçimi desteklenmiyor</translation>
+    </message>
+    <message>
+        <location filename="../feedback.cpp" line="525"/>
+        <source>选择附件</source>
+        <translatorcomment>Dosya Seç</translatorcomment>
+        <translation>Dosya Seç</translation>
+    </message>
+    <message>
+        <source>选择图片</source>
+        <translatorcomment>Fotoğraf seç</translatorcomment>
+        <translation type="obsolete">Fotoğraf seç</translation>
+    </message>
+    <message>
+        <location filename="../feedback.cpp" line="1264"/>
+        <source>删除</source>
+        <translatorcomment>Sil</translatorcomment>
+        <translation>Sil</translation>
+    </message>
+</context>
+<context>
+    <name>submit_fail</name>
+    <message>
+        <location filename="../submit_fail.cpp" line="40"/>
+        <source>提交失败</source>
+        <translatorcomment>Gönderme başarısız</translatorcomment>
+        <translation>Gönderme başarısız</translation>
+    </message>
+    <message>
+        <location filename="../submit_fail.cpp" line="66"/>
+        <source>问题提交失败</source>
+        <translatorcomment>Sorun gönderilemedi</translatorcomment>
+        <translation>Sorun gönderilemedi</translation>
+    </message>
+    <message>
+        <location filename="../submit_fail.cpp" line="81"/>
+        <source>重新发送</source>
+        <translatorcomment>Tekrar Gönder</translatorcomment>
+        <translation>Tekrar Gönder</translation>
+    </message>
+    <message>
+        <location filename="../submit_fail.cpp" line="89"/>
+        <source>退出</source>
+        <translatorcomment>Çıkış</translatorcomment>
+        <translation>Çıkış</translation>
+    </message>
+    <message>
+        <location filename="../submit_fail.cpp" line="113"/>
+        <source>远程服务器拒绝连接</source>
+        <translatorcomment>Uzak sunucu bağlantıyı reddetti</translatorcomment>
+        <translation>Uzak sunucu bağlantıyı reddetti</translation>
+    </message>
+    <message>
+        <location filename="../submit_fail.cpp" line="116"/>
+        <source>服务器关闭</source>
+        <translatorcomment>Sunucu kapalı</translatorcomment>
+        <translation>Sunucu kapalı</translation>
+    </message>
+    <message>
+        <location filename="../submit_fail.cpp" line="119"/>
+        <source>找不到远程主机名（无效的主机名）</source>
+        <translatorcomment>Uzak ana bilgisayar adı bulunamıyor (geçersiz ana bilgisayar adı)</translatorcomment>
+        <translation>Uzak ana bilgisayar adı bulunamıyor (geçersiz ana bilgisayar adı)</translation>
+    </message>
+    <message>
+        <location filename="../submit_fail.cpp" line="122"/>
+        <source>与远程服务器的连接超时</source>
+        <translatorcomment>Uzak sunucuya bağlantı zaman aşımına uğradı</translatorcomment>
+        <translation>Uzak sunucuya bağlantı zaman aşımına uğradı</translation>
+    </message>
+    <message>
+        <location filename="../submit_fail.cpp" line="125"/>
+        <source>网络未连接</source>
+        <translatorcomment>İnternet bağlantısı yok</translatorcomment>
+        <translation>İnternet bağlantısı yok</translation>
+    </message>
+    <message>
+        <location filename="../submit_fail.cpp" line="128"/>
+        <source>未知错误</source>
+        <translatorcomment>Bilinmeyen hata</translatorcomment>
+        <translation>Bilinmeyen hata</translation>
+    </message>
+</context>
+<context>
+    <name>submit_success</name>
+    <message>
+        <location filename="../submit_success.cpp" line="34"/>
+        <source>提交成功 </source>
+        <translatorcomment>Gönderilen başarılı</translatorcomment>
+        <translation>Gönderilen başarılı</translation>
+    </message>
+    <message>
+        <location filename="../submit_success.cpp" line="57"/>
+        <source>问题提交成功</source>
+        <translatorcomment>Gönderilen başarılı</translatorcomment>
+        <translation>Gönderilen başarılı</translation>
+    </message>
+    <message>
+        <location filename="../submit_success.cpp" line="71"/>
+        <source>继续反馈问题</source>
+        <translatorcomment>Devam Et</translatorcomment>
+        <translation>Devam Et</translation>
+    </message>
+    <message>
+        <location filename="../submit_success.cpp" line="79"/>
+        <source>退出</source>
+        <translatorcomment>Çıkış</translatorcomment>
+        <translation>Çıkış</translation>
+    </message>
+</context>
+</TS>
