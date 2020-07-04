a48a5791b58fe0cbad43e850d9b2be2ae80ccf0c
Aylinux <cihanalk@gmail.com>
2020-07-04 09:36:15 +0300

    Create tr.ts (#237)

---
+++ src/plugins/clipboardPlugin/tr.ts
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr_TR">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="cleanpromptbox.cpp" line="67"/>
+        <source>Are you sure empty your clipboard history?</source>
+        <translation>Pano geçmişinizi boşalttığınızdan emin misiniz?</translation>
+    </message>
+    <message>
+        <location filename="cleanpromptbox.cpp" line="81"/>
+        <source>Don&apos;t ask</source>
+        <translation>Sorma</translation>
+    </message>
+    <message>
+        <location filename="cleanpromptbox.cpp" line="94"/>
+        <location filename="editorwidget.cpp" line="102"/>
+        <source>Confirm</source>
+        <translation>Onayla</translation>
+    </message>
+    <message>
+        <location filename="cleanpromptbox.cpp" line="96"/>
+        <location filename="editorwidget.cpp" line="106"/>
+        <source>Cancel</source>
+        <translation>İptal</translation>
+    </message>
+    <message>
+        <location filename="clipboardwidgetentry.cpp" line="93"/>
+        <source>Pop</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="clipboardwidgetentry.cpp" line="101"/>
+        <source>EditButton</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="clipboardwidgetentry.cpp" line="108"/>
+        <source>Remove</source>
+        <translation>Çıkar</translation>
+    </message>
+    <message>
+        <location filename="clipboardwidgetentry.cpp" line="115"/>
+        <source>Cancel the fixed</source>
+        <translation>Düzeltmeyi iptal et</translation>
+    </message>
+    <message>
+        <location filename="editorwidget.cpp" line="80"/>
+        <source>Edit</source>
+        <translation>Düzenle</translation>
+    </message>
+    <message>
+        <location filename="sidebarclipboardplugin.h" line="97"/>
+        <source>ClipBoard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sidebarclipboardplugin.h" line="99"/>
+        <source>Show the folder children as icons.</source>
+        <translation type="unfinished">Klasörleri simgeler olarak gösterin.</translation>
+    </message>
+</context>
+<context>
+    <name>SearchWidgetItemContent</name>
+    <message>
+        <location filename="searchwidgetitemcontent.cpp" line="39"/>
+        <source>Clear</source>
+        <translation>Temizle</translation>
+    </message>
+    <message>
+        <location filename="searchwidgetitemcontent.cpp" line="64"/>
+        <source>search...</source>
+        <translation>Ara...</translation>
+    </message>
+</context>
+<context>
+    <name>SidebarClipboardPlugin</name>
+    <message>
+        <location filename="sidebarclipboardplugin.cpp" line="121"/>
+        <source>No clip content</source>
+        <translation>Klip içeriği yok</translation>
+    </message>
+</context>
+</TS>
