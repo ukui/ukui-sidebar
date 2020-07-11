3c526556452bf2c3ce50249d88d8099dec9314d0
Yue Shuai <yueshuai@kylinos.cn>
2020-07-11 16:43:50 +0800

    Supplement menu in Chinese
    Signed-off-by: Yue Shuai <yueshuai@kylinos.cn>

--- src/plugins/ukui_notebook/ukui_notebook_zh_CN.ts
+++ src/plugins/ukui_notebook/ukui_notebook_zh_CN.ts
@@ -9,19 +9,19 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="edit_page.cpp" line="49"/>
+        <location filename="edit_page.cpp" line="46"/>
         <source>ukui-memo</source>
         <translatorcomment>麒麟便签页</translatorcomment>
         <translation>麒麟便签页</translation>
     </message>
     <message>
-        <location filename="edit_page.cpp" line="751"/>
+        <location filename="edit_page.cpp" line="775"/>
         <source>Close</source>
         <translatorcomment>关闭</translatorcomment>
         <translation>关闭</translation>
     </message>
     <message>
-        <location filename="edit_page.cpp" line="753"/>
+        <location filename="edit_page.cpp" line="777"/>
         <source>Open Notepad</source>
         <translatorcomment>打开便签本</translatorcomment>
         <translation>打开便签本</translation>
@@ -35,22 +35,25 @@
 <context>
     <name>HeaderBar</name>
     <message>
-        <location filename="headerbar.cpp" line="65"/>
-        <source>View Type</source>
+        <location filename="headerbar.cpp" line="67"/>
+        <source>Sort Type</source>
         <translation type="unfinished"></translation>
     </message>
+</context>
+<context>
+    <name>PaletteWidget</name>
     <message>
-        <location filename="headerbar.cpp" line="94"/>
-        <source>Sort Type</source>
+        <location filename="paletteWidget.ui" line="26"/>
+        <source>Form</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
-        <location filename="widget.cpp" line="678"/>
-        <location filename="widget.cpp" line="1026"/>
-        <location filename="widget.cpp" line="1035"/>
+        <location filename="widget.cpp" line="725"/>
+        <location filename="widget.cpp" line="1113"/>
+        <location filename="widget.cpp" line="1122"/>
         <source>%1 records in total</source>
         <translatorcomment>%1条记录</translatorcomment>
         <translation>%1条记录</translation>
@@ -73,87 +76,101 @@
     </message>
 </context>
 <context>
+    <name>TextEditWidget</name>
+    <message>
+        <location filename="textEditWidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="textEditWidget.ui" line="165"/>
+        <source>14</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>Text_editing</name>
     <message>
         <location filename="text_editing.ui" line="14"/>
         <source>Form</source>
         <translation></translation>
     </message>
-    <message>
-        <location filename="text_editing.ui" line="321"/>
-        <source>22</source>
-        <translation></translation>
-    </message>
 </context>
 <context>
     <name>Widget</name>
     <message>
-        <location filename="widget.ui" line="14"/>
+        <location filename="widget.ui" line="26"/>
         <source>Widget</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="widget.ui" line="69"/>
+        <location filename="widget.ui" line="80"/>
         <source>New</source>
         <translatorcomment>新建</translatorcomment>
         <translation>新建</translation>
     </message>
     <message>
-        <location filename="widget.cpp" line="225"/>
+        <location filename="widget.cpp" line="227"/>
         <source>ukui-note</source>
         <translatorcomment>麒麟便签本</translatorcomment>
         <translation>麒麟便签本</translation>
     </message>
     <message>
-        <location filename="widget.cpp" line="232"/>
+        <location filename="widget.cpp" line="838"/>
         <source>Search</source>
         <translatorcomment>搜索</translatorcomment>
         <translation>搜索</translation>
     </message>
     <message>
-        <location filename="widget.cpp" line="412"/>
+        <location filename="widget.cpp" line="453"/>
         <source>Create New Note</source>
         <translatorcomment>新建便签</translatorcomment>
         <translation>新建便签</translation>
     </message>
     <message>
-        <location filename="widget.cpp" line="413"/>
+        <location filename="widget.cpp" line="454"/>
         <source>Delete Selected Note</source>
         <translatorcomment>删除</translatorcomment>
         <translation>删除</translation>
     </message>
     <message>
-        <location filename="widget.cpp" line="414"/>
+        <location filename="widget.cpp" line="455"/>
         <source>Switch View</source>
         <translatorcomment>切换视图</translatorcomment>
         <translation>切换视图</translation>
     </message>
     <message>
-        <location filename="widget.cpp" line="415"/>
+        <location filename="widget.cpp" line="456"/>
         <source>Sort</source>
         <translatorcomment>排序</translatorcomment>
         <translation>排序</translation>
     </message>
     <message>
-        <location filename="widget.cpp" line="416"/>
         <source>Switching Themes</source>
         <translatorcomment>换肤</translatorcomment>
-        <translation>换肤</translation>
+        <translation type="vanished">换肤</translation>
     </message>
     <message>
-        <location filename="widget.cpp" line="417"/>
+        <location filename="widget.cpp" line="457"/>
         <source>Exit</source>
         <translatorcomment>关闭</translatorcomment>
         <translation>关闭</translation>
     </message>
     <message>
-        <location filename="widget.cpp" line="418"/>
+        <location filename="widget.cpp" line="458"/>
         <source>Mini</source>
         <translatorcomment>最小化</translatorcomment>
         <translation>最小化</translation>
     </message>
     <message>
-        <location filename="widget.cpp" line="726"/>
+        <location filename="widget.cpp" line="459"/>
+        <source>Menu</source>
+        <translatorcomment>菜单</translatorcomment>
+        <translation type="unfinished">菜单</translation>
+    </message>
+    <message>
+        <location filename="widget.cpp" line="773"/>
+        <location filename="widget.cpp" line="815"/>
         <source>Welcome to use Kylin system, you will experience more intimate service.</source>
         <translatorcomment>欢迎使用优麒麟系统，您将体会到更贴心的服务。</translatorcomment>
         <translation>欢迎使用优麒麟系统，您将体会到更贴心的服务。</translation>
@@ -198,13 +215,13 @@
 <context>
     <name>iconViewModeDelegate</name>
     <message>
-        <location filename="iconviewmodedelegate.cpp" line="343"/>
+        <location filename="iconviewmodedelegate.cpp" line="418"/>
         <source>Today </source>
         <translatorcomment>今天</translatorcomment>
         <translation>今天</translation>
     </message>
     <message>
-        <location filename="iconviewmodedelegate.cpp" line="347"/>
+        <location filename="iconviewmodedelegate.cpp" line="422"/>
         <source>Yesterday </source>
         <translatorcomment>昨天</translatorcomment>
         <translation>昨天</translation>
@@ -213,13 +230,13 @@
 <context>
     <name>listViewModeDelegate</name>
     <message>
-        <location filename="listViewModeDelegate.cpp" line="373"/>
+        <location filename="listViewModeDelegate.cpp" line="453"/>
         <source>Today </source>
         <translatorcomment>今天</translatorcomment>
         <translation>今天</translation>
     </message>
     <message>
-        <location filename="listViewModeDelegate.cpp" line="377"/>
+        <location filename="listViewModeDelegate.cpp" line="457"/>
         <source>Yesterday </source>
         <translatorcomment>昨天</translatorcomment>
         <translation>昨天</translation>
@@ -228,30 +245,30 @@
 <context>
     <name>noteExitWindow</name>
     <message>
-        <location filename="noteExitWindow.ui" line="14"/>
+        <location filename="noteExitWindow.ui" line="19"/>
         <source>Dialog</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="noteExitWindow.ui" line="44"/>
+        <location filename="noteExitWindow.ui" line="71"/>
         <source>Are you sure to exit the note book?</source>
         <translatorcomment>确认要退出便签本吗?</translatorcomment>
         <translation>确认要退出便签本吗?</translation>
     </message>
     <message>
-        <location filename="noteExitWindow.ui" line="64"/>
+        <location filename="noteExitWindow.ui" line="98"/>
         <source>Close the desktop note page at the same time</source>
         <translatorcomment>同时关闭桌面便签页</translatorcomment>
         <translation>同时关闭桌面便签页</translation>
     </message>
     <message>
-        <location filename="noteExitWindow.ui" line="81"/>
+        <location filename="noteExitWindow.ui" line="114"/>
         <source>No</source>
         <translatorcomment>取消</translatorcomment>
         <translation>取消</translation>
     </message>
     <message>
-        <location filename="noteExitWindow.ui" line="98"/>
+        <location filename="noteExitWindow.ui" line="130"/>
         <source>Yes</source>
         <translatorcomment>确定</translatorcomment>
         <translation>确定</translation>
