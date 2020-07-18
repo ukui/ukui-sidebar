2d3e1cbdc39638c3a81ac2d5bae0a6e90cf96d1e
wanghailiang <wanghailiang@kylinos.cn>
2020-07-18 19:34:35 +0800

    Increase screen resolution judgment

--- src/main.cpp
+++ src/main.cpp
@@ -37,10 +37,12 @@
 #include "mostgrandwidget.h"
 int main(int argc, char *argv[])
 {
+    if(QApplication::desktop()->width() >= 2560){
 #if (QT_VERSION >= QT_VERSION_CHECK(5, 6, 0))
-   QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-   QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+        QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+        QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
 #endif
+    }
     /* 如果系统中有实例在运行则退出 */
     QStringList strlistHomePath = QStandardPaths::standardLocations(QStandardPaths::HomeLocation);
     QString strLockPath = strlistHomePath.at(0) + "/.config/ukui-sidebar";
--- src/plugins/feedback/main.cpp
+++ src/plugins/feedback/main.cpp
@@ -18,13 +18,15 @@
 #include "feedback.h"
 
 #include "singleApplication.h"
-
+#include <QDesktopWidget>
 int main(int argc, char *argv[])
 {
+    if(QApplication::desktop()->width() >= 2560){
 #if (QT_VERSION >= QT_VERSION_CHECK(5, 6, 0))
-   QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-   QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+        QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+        QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
 #endif
+    }
     SingleApplication a(argc, argv);
     if(!a.isRunning()){
         feedback w;
