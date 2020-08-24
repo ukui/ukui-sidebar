e572d7be096008fcb014f3b8c4e46d7fd25c78ef
liyanda <liyanda@qq.com>
2020-08-24 16:10:52 +0800

    Modify transparency

--- src/plugins/ukui-sidebar-notification/External_Widget.cpp
+++ src/plugins/ukui-sidebar-notification/External_Widget.cpp
@@ -4,6 +4,18 @@
 external_widget::external_widget()
 {
 
+    initGsettingTransparency();
+}
+
+void external_widget::initGsettingTransparency()
+{
+    if (QGSettings::isSchemaInstalled(UKUI_TRANSPARENCY_SETTING)) {
+        m_pTransparency = new QGSettings(UKUI_TRANSPARENCY_SETTING);
+        m_dTranSparency = m_pTransparency->get("transparency").toDouble();
+        connect(m_pTransparency, &QGSettings::changed, this, [=](QString value) {
+            m_dTranSparency = m_pTransparency->get("transparency").toDouble();;
+        });
+    }
 }
 
 void external_widget::paintEvent(QPaintEvent *e)
@@ -16,8 +28,7 @@ void external_widget::paintEvent(QPaintEvent *e)
 
     p.setRenderHint(QPainter::Antialiasing);  // 反锯齿;
     p.setBrush(QBrush(QColor(20,20,20)));
-
-    p.setOpacity(0.7);
+    p.setOpacity(m_dTranSparency);
     p.setPen(Qt::NoPen);
     p.drawRoundedRect(rect,0,0);
 
--- src/plugins/ukui-sidebar-notification/External_Widget.h
+++ src/plugins/ukui-sidebar-notification/External_Widget.h
@@ -3,10 +3,16 @@
 
 #include <QWidget>
 #include <QPainter>
+#include <QGSettings>
+
+#define UKUI_TRANSPARENCY_SETTING "org.ukui.control-center.personalise"
 class external_widget:public QWidget
 {
 public:
     external_widget();
+    QGSettings   *m_pTransparency;
+    double        m_dTranSparency = 0.7;
+    void  initGsettingTransparency();
     void paintEvent(QPaintEvent *e);                //重绘事件
 };
 
