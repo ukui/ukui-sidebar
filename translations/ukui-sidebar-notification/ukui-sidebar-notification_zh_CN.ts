d07804677a18bb30446865acafe93eaa5f12fe2e
liushanwen1997 <773996719@qq.com>
2020-09-24 17:07:05 +0800

    add notification translations

--- translations/ukui-sidebar-notification/ukui-sidebar-notification_zh_CN.ts
+++ translations/ukui-sidebar-notification/ukui-sidebar-notification_zh_CN.ts
@@ -4,38 +4,42 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../../src/plugins/ukui-sidebar-notification/notificationPlugin.cpp" line="65"/>
+        <location filename="../../src/plugins/ukui-sidebar-notification/notificationPlugin.cpp" line="59"/>
         <source>Notification center</source>
         <translation type="unfinished">通知中心</translation>
     </message>
     <message>
-        <location filename="../../src/plugins/ukui-sidebar-notification/notificationPlugin.cpp" line="112"/>
-        <location filename="../../src/plugins/ukui-sidebar-notification/notificationPlugin.cpp" line="681"/>
+        <location filename="../../src/plugins/ukui-sidebar-notification/notificationPlugin.cpp" line="650"/>
         <source>Important notice</source>
         <translation type="unfinished">重要的通知</translation>
     </message>
     <message>
-        <location filename="../../src/plugins/ukui-sidebar-notification/notificationPlugin.cpp" line="120"/>
+        <location filename="../../src/plugins/ukui-sidebar-notification/notificationPlugin.cpp" line="108"/>
         <source>Clean up</source>
         <translation type="unfinished">清空</translation>
     </message>
     <message>
-        <location filename="../../src/plugins/ukui-sidebar-notification/notificationPlugin.cpp" line="151"/>
+        <location filename="../../src/plugins/ukui-sidebar-notification/notificationPlugin.cpp" line="100"/>
+        <source>Recent news</source>
+        <translation>最近消息</translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-notification/notificationPlugin.cpp" line="115"/>
         <source>Set up</source>
         <translation type="unfinished">设置</translation>
     </message>
     <message>
-        <location filename="../../src/plugins/ukui-sidebar-notification/notificationPlugin.cpp" line="217"/>
+        <location filename="../../src/plugins/ukui-sidebar-notification/notificationPlugin.cpp" line="171"/>
         <source>No new notifications</source>
         <translation type="unfinished">没有新通知</translation>
     </message>
     <message>
-        <location filename="../../src/plugins/ukui-sidebar-notification/notificationPlugin.cpp" line="240"/>
+        <location filename="../../src/plugins/ukui-sidebar-notification/notificationPlugin.cpp" line="194"/>
         <source>No unimportant notice</source>
         <translation type="unfinished">没有不重要的通知</translation>
     </message>
     <message>
-        <location filename="../../src/plugins/ukui-sidebar-notification/notificationPlugin.cpp" line="653"/>
+        <location filename="../../src/plugins/ukui-sidebar-notification/notificationPlugin.cpp" line="622"/>
         <source>Unimportant notice</source>
         <translation type="unfinished">不重要的通知</translation>
     </message>
@@ -43,22 +47,22 @@
 <context>
     <name>SingleMsg</name>
     <message>
-        <location filename="../../src/plugins/ukui-sidebar-notification/singlemsg.cpp" line="116"/>
+        <location filename="../../src/plugins/ukui-sidebar-notification/singlemsg.cpp" line="129"/>
         <source>now</source>
         <translation type="unfinished">现在</translation>
     </message>
     <message>
-        <location filename="../../src/plugins/ukui-sidebar-notification/singlemsg.cpp" line="284"/>
+        <location filename="../../src/plugins/ukui-sidebar-notification/singlemsg.cpp" line="344"/>
         <source>Yesterday </source>
         <translation type="unfinished">昨天 </translation>
     </message>
     <message>
-        <location filename="../../src/plugins/ukui-sidebar-notification/singlemsg.cpp" line="363"/>
+        <location filename="../../src/plugins/ukui-sidebar-notification/singlemsg.cpp" line="423"/>
         <source>In addition </source>
         <translation type="unfinished">还有 </translation>
     </message>
     <message>
-        <location filename="../../src/plugins/ukui-sidebar-notification/singlemsg.cpp" line="363"/>
+        <location filename="../../src/plugins/ukui-sidebar-notification/singlemsg.cpp" line="423"/>
         <source> notification</source>
         <translation type="unfinished"> 则通知</translation>
     </message>
@@ -70,12 +74,12 @@
 <context>
     <name>TakeInBoxToolButton</name>
     <message>
-        <location filename="../../src/plugins/ukui-sidebar-notification/takeinboxtoolbutton.cpp" line="36"/>
+        <location filename="../../src/plugins/ukui-sidebar-notification/takeinboxtoolbutton.cpp" line="37"/>
         <source>Enter unimportant news</source>
         <translation type="unfinished">进入不重要通知</translation>
     </message>
     <message>
-        <location filename="../../src/plugins/ukui-sidebar-notification/takeinboxtoolbutton.cpp" line="41"/>
+        <location filename="../../src/plugins/ukui-sidebar-notification/takeinboxtoolbutton.cpp" line="43"/>
         <source>Quit unimportant news</source>
         <translation type="unfinished">退出不重要通知</translation>
     </message>
