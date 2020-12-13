79e55c98b1e235e4a0cafcb6fafe975168fcaa4d
roxfr <roxfr@outlook.fr>
2020-12-13 18:04:48 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: ukui-sidebar/sidebar
    Translate-URL: https://weblate.ubuntukylin.com/projects/ukui-sidebar/sidebar/fr/

--- translations/ukui-sidebar_fr.ts
+++ translations/ukui-sidebar_fr.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../src/main.cpp" line="81"/>
         <source>Display debug information</source>
-        <translation type="unfinished"></translation>
+        <translation>Afficher les informations de débogage</translation>
     </message>
     <message>
         <location filename="../src/widget.cpp" line="177"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>Ouvrir</translation>
     </message>
     <message>
         <location filename="../src/widget.cpp" line="180"/>
         <source>Set up notification center</source>
-        <translation type="unfinished"></translation>
+        <translation>Configurer le centre de notification</translation>
     </message>
     <message>
         <location filename="../src/sidebarpluginswidgets.cpp" line="112"/>
         <source>折叠</source>
-        <translation type="unfinished"></translation>
+        <translation>Plier</translation>
     </message>
     <message>
         <location filename="../src/sidebarpluginswidgets.cpp" line="114"/>
         <source>展开</source>
-        <translation type="unfinished"></translation>
+        <translation>Se dérouler</translation>
     </message>
 </context>
 <context>
@@ -34,7 +34,7 @@
     <message>
         <location filename="../src/widget.cpp" line="210"/>
         <source>Sidebar</source>
-        <translation type="unfinished"></translation>
+        <translation>Barre latérale</translation>
     </message>
 </context>
 <context>
@@ -42,12 +42,12 @@
     <message>
         <location filename="../src/sidebarpluginswidgets.cpp" line="97"/>
         <source>Clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Presse-papiers</translation>
     </message>
     <message>
         <location filename="../src/sidebarpluginswidgets.cpp" line="104"/>
         <source>Plugins</source>
-        <translation type="unfinished"></translation>
+        <translation>Plugins</translation>
     </message>
 </context>
 </TS>
