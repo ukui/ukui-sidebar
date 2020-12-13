253b30c9a6ea571a23506c7b7b77ed0fdc15a56a
Weblate <noreply@weblate.org>
2020-12-13 11:05:35 +0000

    Added translation using Weblate (Tibetan)

---
+++ translations/ukui-sidebar-notification/ukui-sidebar-notification_bo_CN.ts
@@ -0,0 +1,83 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="bo_CN">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-notification/notificationPlugin.cpp" line="65"/>
+        <source>Notification center</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-notification/notificationPlugin.cpp" line="112"/>
+        <location filename="../../src/plugins/ukui-sidebar-notification/notificationPlugin.cpp" line="681"/>
+        <source>Important notice</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-notification/notificationPlugin.cpp" line="120"/>
+        <source>Clean up</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-notification/notificationPlugin.cpp" line="151"/>
+        <source>Set up</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-notification/notificationPlugin.cpp" line="217"/>
+        <source>No new notifications</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-notification/notificationPlugin.cpp" line="240"/>
+        <source>No unimportant notice</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-notification/notificationPlugin.cpp" line="653"/>
+        <source>Unimportant notice</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SingleMsg</name>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-notification/singlemsg.cpp" line="116"/>
+        <source>now</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-notification/singlemsg.cpp" line="284"/>
+        <source>Yesterday </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-notification/singlemsg.cpp" line="363"/>
+        <source>In addition </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-notification/singlemsg.cpp" line="363"/>
+        <source> notification</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>In addition</source>
+        <translation type="obsolete">Tür</translation>
+    </message>
+</context>
+<context>
+    <name>TakeInBoxToolButton</name>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-notification/takeinboxtoolbutton.cpp" line="36"/>
+        <source>Enter unimportant news</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-notification/takeinboxtoolbutton.cpp" line="41"/>
+        <source>Quit unimportant news</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
