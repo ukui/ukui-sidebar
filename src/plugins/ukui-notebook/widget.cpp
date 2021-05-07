fc9eda06ed2f78f9b62b9708678db6f795bb7f9a
datyuesh <yueshuai@kylinos.cn>
2021-05-07 10:04:03 +0800

    Fix: User manual interface call modification
    Signed-off-by: datyuesh <yueshuai@kylinos.cn>

--- src/plugins/ukui-notebook/widget.cpp
+++ src/plugins/ukui-notebook/widget.cpp
@@ -554,7 +554,7 @@ void Widget::btnInit()
 
     connect(m_helpAction, &QAction::triggered, this, [=](){
         qDebug() << "help clicked";
-        userGuideInterface->call(QString("showGuide"), "tools/ukui-notebook");
+        userGuideInterface->call(QString("showGuide"), "ukui/ukui-notebook");
     });
     connect(m_aboutAction, &QAction::triggered, this, [=](){
         About *dialog = new About();
