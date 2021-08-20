488988044bd060d8e4b5f1778e9aaa8680d19a7e
liyanda <2772454664@qq.com>
2021-08-20 14:49:37 +0800

    删减剪贴板的横向滑动条

--- src/plugins/ukui-sidebar-clipboard/sidebarclipboardplugin.cpp
+++ src/plugins/ukui-sidebar-clipboard/sidebarclipboardplugin.cpp
@@ -133,9 +133,9 @@ void SidebarClipboardPlugin::createWidget()
 
     m_pShortcutOperationListWidget = new ClipBoardLisetWidget;
     m_pShortcutOperationListWidget->verticalScrollBar()->setProperty("drawScrollBarGroove", false);
-    m_pShortcutOperationListWidget->horizontalScrollBar()->setProperty("drawScrollBarGroove", false);
+    m_pShortcutOperationListWidget->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);       //隐藏横向滚动条
     m_pShortcutOperationListWidget->setContentsMargins(0,0,0,0);
-    m_pShortcutOperationListWidget->setFixedSize(400,230);
+    m_pShortcutOperationListWidget->setFixedSize(400,240);
 
     m_pSearchWidgetListWidget      = new QListWidget;
     m_pSearchWidgetListWidget->setFixedSize(400, 50);
