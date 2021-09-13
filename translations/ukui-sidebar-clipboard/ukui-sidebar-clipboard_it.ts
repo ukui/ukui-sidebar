858c7e041ed7db91265acc86ee3fc9b76a6a148f
Weblate <noreply@weblate.org>
2021-09-13 14:37:52 +0000

    Added translation using Weblate (Italian)

---
+++ translations/ukui-sidebar-clipboard/ukui-sidebar-clipboard_it.ts
@@ -0,0 +1,133 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-clipboard/cleanpromptbox.cpp" line="84"/>
+        <source>Are you sure empty your clipboard history?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-clipboard/cleanpromptbox.cpp" line="99"/>
+        <source>Don&apos;t ask</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-clipboard/cleanpromptbox.cpp" line="113"/>
+        <location filename="../../src/plugins/ukui-sidebar-clipboard/editorwidget.cpp" line="108"/>
+        <source>Confirm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-clipboard/cleanpromptbox.cpp" line="115"/>
+        <location filename="../../src/plugins/ukui-sidebar-clipboard/editorwidget.cpp" line="112"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-clipboard/clipboardwidgetentry.cpp" line="93"/>
+        <source>Pop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-clipboard/clipboardwidgetentry.cpp" line="101"/>
+        <source>EditButton</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-clipboard/clipboardwidgetentry.cpp" line="108"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-clipboard/clipboardwidgetentry.cpp" line="115"/>
+        <source>Cancel the fixed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-clipboard/editorwidget.cpp" line="31"/>
+        <source>edit box</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-clipboard/editorwidget.cpp" line="84"/>
+        <source>Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-clipboard/sidebarclipboardplugin.h" line="97"/>
+        <source>ClipBoard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-clipboard/sidebarclipboardplugin.h" line="99"/>
+        <source>Show the folder children as icons.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QWidgetTextControl</name>
+    <message>
+        <source>&amp;Undo</source>
+        <translatorcomment>ཕྱིར་འཐེན། </translatorcomment>
+        <translation type="vanished">ཕྱིར་འཐེན། </translation>
+    </message>
+    <message>
+        <source>&amp;Redo</source>
+        <translatorcomment>སླར་གསོ། </translatorcomment>
+        <translation type="vanished">སླར་གསོ། </translation>
+    </message>
+    <message>
+        <source>Cu&amp;t</source>
+        <translatorcomment>དྲས་སྦྱར་</translatorcomment>
+        <translation type="vanished">དྲས་སྦྱར་</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translatorcomment>འདྲ་བཟོ་</translatorcomment>
+        <translation type="vanished">འདྲ་བཟོ་</translation>
+    </message>
+    <message>
+        <source>Copy &amp;Link Location</source>
+        <translatorcomment>འདྲ་ཕབ་འབྲེལ་མཐུད།</translatorcomment>
+        <translation type="vanished">འདྲ་ཕབ་འབྲེལ་མཐུད།</translation>
+    </message>
+    <message>
+        <source>&amp;Paste</source>
+        <translatorcomment>སྦྱར་བ་</translatorcomment>
+        <translation type="vanished">སྦྱར་བ་</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translatorcomment>སུབ་པ་</translatorcomment>
+        <translation type="vanished">སུབ་པ་</translation>
+    </message>
+    <message>
+        <source>Select All</source>
+        <translatorcomment>ཡོངས་འདེམས་</translatorcomment>
+        <translation type="vanished">ཡོངས་འདེམས་</translation>
+    </message>
+</context>
+<context>
+    <name>SearchWidgetItemContent</name>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-clipboard/searchwidgetitemcontent.cpp" line="31"/>
+        <source>Clear</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-clipboard/searchwidgetitemcontent.cpp" line="56"/>
+        <source>search...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SidebarClipboardPlugin</name>
+    <message>
+        <location filename="../../src/plugins/ukui-sidebar-clipboard/sidebarclipboardplugin.cpp" line="125"/>
+        <source>No clip content</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
